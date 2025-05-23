cb9aba5eb90e098fa38b32142bea66643a9df15b
sy0tom <hashimoto.sho@linecorp.com>
2025-05-23 09:12:46 +0900

    Change entrypoint

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig, WxtViteConfig } from "wxt";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { defineConfig, WxtViteConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  entrypointsDir: "src/apps",
   vite: (): WxtViteConfig | Promise<WxtViteConfig> => ({
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "#": path.resolve(__dirname, "src"),
+      },
+    },
   }),
 });
